0fb78a7ed5c5e57d5e8d9438a971bd439dafd5e0
isisarsilv <136837157+isisarsilv@users.noreply.github.com>
2024-10-16 01:00:52 -0300

    Add files via upload

---
+++ js/banner2.js
@@ -0,0 +1,8 @@
+var imagens = ["img/diversidads.JPG", "img/diversidade.JPG", "img/divers", "img/ddssaddas.JPG"];
+var imagematual = 0;
+
+function trocaimagem() {
+    imagematual = (imagematual + 1) % 4;
+    document.querySelector('.JPG img').src = imagens[imagematual];
+}
+setInterval(trocaimagem, 1500);
