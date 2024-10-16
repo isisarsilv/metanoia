871ba5de907e04a6637d1f665b34e4a52e94767d
isisarsilv <136837157+isisarsilv@users.noreply.github.com>
2024-10-15 21:39:47 -0300

    Delete js directory
    weqweq

--- js/banner.js
+++
@@ -1,8 +0,0 @@
-var imagens = ["img/banner2.JPG", "img/banner3.JPG", "img/banner4.JPG", "img/banner5.JPG"];
-var imagematual = 0;
-
-function trocaimagem() {
-    imagematual = (imagematual + 1) % 4;
-    document.querySelector('.JPG img').src = imagens[imagematual];
-}
-setInterval(trocaimagem, 1500);
